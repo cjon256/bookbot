719d8a6c45d1b7fd2d6de061d472db8fda579079
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2023-11-18 23:03:28 -0500

    Added header to say it is a report.
    mostly so it will look like the result on the boot.dev site....

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ path_to_file = "books/frankenstein.txt"
 with open(path_to_file) as f:
     file_contents = f.read()
 
+print(f"--- Begin report of {path_to_file} ---")
 words = file_contents.split()
 print(len(words))
 
