448c900791971f9592f00c51af5109512567484d
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2023-11-18 22:58:53 -0500

    Hacked up version of the test project for boot.dev
    It's really more about getting comfortable with git I think
    so there was not much effort made to make quality code.

---
+++ main.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+path_to_file = "books/frankenstein.txt"
+
+with open(path_to_file) as f:
+    file_contents = f.read()
+
+words = file_contents.split()
+print(len(words))
+
+# probably more obscure than it needs to be
+letter_frequency = Counter(
+    map(lambda c: c.lower(), filter(lambda c: c.isalpha(), [*file_contents])))
+for letter, count in letter_frequency.most_common():
+    print(f"The '{letter}' character was found {count} times.")
